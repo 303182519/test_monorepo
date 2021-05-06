6e4f5369307fdf7932fa4214c8377522d6e89d0d
xiejinlong <xiejinlong@huya.com>
2021-05-06 11:14:58 +0800

    feat(ui-lib): 2.3.0

--- packages/ui-lib/src/index.ts
+++ packages/ui-lib/src/index.ts
@@ -9,7 +9,7 @@ export function multi(a:number,b:number){
   return lodash.multiply(a,b);
 }
 export function divide(a:number, b:number){
-  console.log('2.1.0');
+  console.log('2.3.0');
   return a/b;
 }
 export function minus(a:number,b:number){
